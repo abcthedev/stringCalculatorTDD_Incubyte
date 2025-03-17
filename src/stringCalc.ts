9e43b0f3a413786888f954d31b9c88fa7d1faddd
abcthedev <bhaskarchowdary2u@gmail.com>
2025-03-18 00:58:27 +0530

    passed

--- src/stringCalc.ts
+++ src/stringCalc.ts
@@ -2,5 +2,5 @@ export const addString = (values: string): number => {
     if (values === "") {
         return 0
     }
-    return values.split(",").reduce((add, num) => add + Number(num), 0)
+    return values.split(/[\n,]/).reduce((add, num) => add + Number(num), 0)
 } 
