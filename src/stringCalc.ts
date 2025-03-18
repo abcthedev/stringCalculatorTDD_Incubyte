db426473424152e15b4c02516393371ef587f41a
abcthedev <bhaskarchowdary2u@gmail.com>
2025-03-18 06:45:09 +0530

    passed

--- src/stringCalc.ts
+++ src/stringCalc.ts
@@ -10,6 +10,9 @@ export const addString = (values: string): number => {
         delimiter = new RegExp(parts[0].slice(2));
         values = parts[1];
     }
+    
+    const negatives = values.split(delimiter).filter((num) => Number(num) < 0);
+    if (negatives.length > 0) throw new Error(`Negative numbers not allowed: ${negatives.join(",")}`);
 
     return values.split(delimiter).reduce((add, num) => add + Number(num), 0)
 } 
